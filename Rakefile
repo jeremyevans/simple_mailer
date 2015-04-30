93373f5e453227a05ba611c229afc108367d8ba3
Jeremy Evans <code@jeremyevans.net>
2015-04-30 15:49:44 -0700

    Convert from rspec to minitest/spec

--- Rakefile
+++ Rakefile
@@ -24,29 +24,8 @@ task :package do
 end
 
 desc "Run specs"
-task :default=>[:spec]
-
-begin
-  begin
-    raise LoadError if ENV['RSPEC1']
-    # RSpec 2
-    require "rspec/core/rake_task"
-    spec_class = RSpec::Core::RakeTask
-    spec_files_meth = :pattern=
-  rescue LoadError
-    # RSpec 1
-    require "spec/rake/spectask"
-    spec_class = Spec::Rake::SpecTask
-    spec_files_meth = :spec_files=
-  end
-
-  desc "Run specs"
-  spec_class.new("spec") do |t|
-    t.send(spec_files_meth, ["spec/*.rb"])
-  end
-rescue LoadError
-  task :default do
-    puts "Must install rspec to run the default task (which runs specs)"
-  end
+task :spec do
+  sh %{#{FileUtils::RUBY} -rubygems -I lib spec/*.rb}
 end
 
+task :default => :spec
--- spec/simple_mailer.rb
+++ spec/simple_mailer.rb
@@ -1,17 +1,8 @@
-#!/usr/bin/env spec
 require File.join(File.dirname(File.dirname(__FILE__)), '/lib/simple_mailer')
 Object.send(:remove_const, :Net)
 
-if defined?(RSpec)
-  require 'rspec/version'
-  if RSpec::Version::STRING >= '2.11.0'
-    RSpec.configure do |config|
-      config.expect_with :rspec do |c|
-        c.syntax = :should
-      end
-    end
-  end
-end
+gem 'minitest'
+require 'minitest/autorun'
 
 $message = [nil, nil, nil]
 module Net
@@ -30,14 +21,20 @@ module Net
   end
 end
 
-shared_examples_for "simple_mailer" do
+module SimpleMailerSpecs
+  extend Minitest::Spec::DSL
+
   before do
     $message = [nil, nil, nil]
   end
+  after do
+    @mailer.smtp_server = nil
+    SimpleMailer.instance_variable_set(:@test_mode, false)
+  end
 
-  it "should sent email" do
+  it "should send email" do
     @mailer.send_email('from1@from.com', 'to1@to.com', 'Test Subject 1', 'Test Body 1')
-    $message.should == [<<END_MESSAGE, 'from1@from.com', 'to1@to.com', 'localhost']
+    $message.must_equal [<<END_MESSAGE, 'from1@from.com', 'to1@to.com', 'localhost']
 From: from1@from.com
 To: to1@to.com
 Subject: Test Subject 1
@@ -48,7 +45,7 @@ END_MESSAGE
 
   it "should allow the setting of headers" do
     @mailer.send_email('from2@from.com', 'to2@to.com', 'Test Subject 2', 'Test Body 2', 'HeaderKey2'=>'HeaderValue2')
-    $message.should == [<<END_MESSAGE, 'from2@from.com', 'to2@to.com', 'localhost']
+    $message.must_equal [<<END_MESSAGE, 'from2@from.com', 'to2@to.com', 'localhost']
 From: from2@from.com
 To: to2@to.com
 Subject: Test Subject 2
@@ -60,7 +57,7 @@ END_MESSAGE
 
   it "should recognize the special :cc header" do
     @mailer.send_email('from3@from.com', 'to3@to.com', 'Test Subject 3', 'Test Body 3', :cc=>'cc3@to.com')
-    $message.should == [<<END_MESSAGE, 'from3@from.com', ['to3@to.com', 'cc3@to.com'], 'localhost']
+    $message.must_equal [<<END_MESSAGE, 'from3@from.com', ['to3@to.com', 'cc3@to.com'], 'localhost']
 From: from3@from.com
 To: to3@to.com
 Subject: Test Subject 3
@@ -72,7 +69,7 @@ END_MESSAGE
 
   it "should recognize the special :bcc header" do
     @mailer.send_email('from3@from.com', 'to3@to.com', 'Test Subject 3', 'Test Body 3', :bcc=>'cc3@to.com')
-    $message.should == [<<END_MESSAGE, 'from3@from.com', ['to3@to.com', 'cc3@to.com'], 'localhost']
+    $message.must_equal [<<END_MESSAGE, 'from3@from.com', ['to3@to.com', 'cc3@to.com'], 'localhost']
 From: from3@from.com
 To: to3@to.com
 Subject: Test Subject 3
@@ -83,7 +80,7 @@ END_MESSAGE
 
   it "should recognize the special :smtp_from and :smtp_to headers" do
     @mailer.send_email('from3@from.com', 'to3@to.com', 'Test Subject 3', 'Test Body 3', 'HeaderKey3'=>'HeaderValue3', :smtp_from=>'from@to.com', :smtp_to=>'to@from.com')
-    $message.should == [<<END_MESSAGE, 'from@to.com', 'to@from.com', 'localhost']
+    $message.must_equal [<<END_MESSAGE, 'from@to.com', 'to@from.com', 'localhost']
 From: from3@from.com
 To: to3@to.com
 Subject: Test Subject 3
@@ -96,13 +93,13 @@ END_MESSAGE
   it "should not modify input hash" do
     h = {:smtp_to=>'to@to.com'}
     @mailer.send_email('from3@from.com', 'to3@to.com', 'Test Subject 3', 'Test Body 3', h)
-    h.should == {:smtp_to=>'to@to.com'}
+    h.must_equal(:smtp_to=>'to@to.com')
   end
 
   it "should allow the setting of smtp server" do
     @mailer.smtp_server = 'blah.com'
     @mailer.send_email('from1@from.com', 'to1@to.com', 'Test Subject 1', 'Test Body 1')
-    $message.should == [<<END_MESSAGE, 'from1@from.com', 'to1@to.com', 'blah.com']
+    $message.must_equal [<<END_MESSAGE, 'from1@from.com', 'to1@to.com', 'blah.com']
 From: from1@from.com
 To: to1@to.com
 Subject: Test Subject 1
@@ -114,13 +111,13 @@ END_MESSAGE
   it "should not send emails in test mode" do
     SimpleMailer.test_mode!
     @mailer.send_email('from3@from.com', 'to3@to.com', 'Test Subject 3', 'Test Body 3', 'HeaderKey3'=>'HeaderValue3', :smtp_from=>'from@to.com', :smtp_to=>'to@from.com')
-    $message.should == [nil, nil, nil]
+    $message.must_equal [nil, nil, nil]
   end
 
   it "should record emails sent to emails_sent in test mode" do
     SimpleMailer.test_mode!
     @mailer.send_email('from3@from.com', 'to3@to.com', 'Test Subject 3', 'Test Body 3', 'HeaderKey3'=>'HeaderValue3', :smtp_from=>'from@to.com', :smtp_to=>'to@from.com')
-    @mailer.emails_sent.should == [[<<END_MESSAGE, 'from@to.com', 'to@from.com']]
+    @mailer.emails_sent.must_equal [[<<END_MESSAGE, 'from@to.com', 'to@from.com']]
 From: from3@from.com
 To: to3@to.com
 Subject: Test Subject 3
@@ -134,10 +131,10 @@ end
 
 describe "SimpleMailer module itself" do
   before{@mailer = SimpleMailer}
-  it_should_behave_like "simple_mailer"
+  include SimpleMailerSpecs
 end
 
 describe "Class including SimpleMailer" do
   before{@mailer = Class.new{include SimpleMailer}.new}
-  it_should_behave_like "simple_mailer"
+  include SimpleMailerSpecs
 end
